784c2193f74964fff67772943cb14d71ba3446f3
yathish95 <naturiyathishreddy@gmail.com>
2022-06-08 11:00:35 +0530

    three

--- truejet/src/main/java/com/truejet/App2.java
+++ truejet/src/main/java/com/truejet/App2.java
@@ -5,6 +5,10 @@ public class App2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Namsthe");
+System.out.println("developer2");
+
+
+
 	}
 
 }
